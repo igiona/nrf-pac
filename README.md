4426e4767d747f3db2f8172171010277c5b38e70
Giona Imperatori <giona.imperatori@gmail.com>
2024-09-22 21:31:34 +0200

    feat(update.sh) : added generator for multiple chips

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # nrf-pac
+
+This is a [Peripheral Access Crate](https://rust-embedded.github.io/book/start/registers.html) for Nordic Semiconductor nRF microcontrollers.
+
+This crate has been automatically generated from the SVD files in [Nordic nrfx](https://github.com/NordicSemiconductor/nrfx/blob/master/mdk/), using [chiptool](https://github.com/embassy-rs/chiptool/). Fixes are added to the SVD file (via YAML file) to make the crate more amenable to writing HALs with, such as converting sets of identical registers/fields to arrays, merging identical registers and enums, etc.
+
+This crate is used for the [`embassy-nrf`](github.com/embassy-rs/embassy/) Rust Hardware Abstraction Layer (HAL) for Nordic Semiconductor nRF microcontrollers.
+
+## Supported chips
+
+- **nrf52840**: [Datasheet](https://docs-be.nordicsemi.com/bundle/ps_nrf52840/attach/nRF52840_PS_v1.10.pdf?_LANG=enus)
+
+### Add support for a new chip
+
+The following steps are required in order to add support for a new chip with name `<name>`:
+
+- add the `<chip-identifier>.svd` and `<chip-identifier>.yaml` files into the `svd` folder
+- add the `device-<chip-identifier>.x` file into the root folder
+- append `<name>` to the `identifiers` associative array in `update.sh`, and set the value to `<chip-identifier>`
+- append `<name>` to the `targets` associative array in `update.sh`, and set the value to appropriate CPU target value
+- update this README file with the newly supported chip information
+- run `update.sh`
+- create PR with the updated files
+
+## License
+
+The contents of this crate are auto-generated and licensed under the same terms as the underlying SVD file, which is licensed by Nordic Semiconductor ASA under a BSD-3-Clause license.
--- update.sh
+++ update.sh
@@ -1,18 +1,94 @@
 #!/usr/bin/env bash
 
+if ! command -v chiptool &> /dev/null; then
+    echo "chiptool could not be found. Install it with the following command:"
+    echo ""
+    echo "    cargo install --git https://github.com/embassy-rs/chiptool --locked"
+    echo ""
+    exit 1
+fi
+
+if ! command -v form &> /dev/null; then
+    echo "form could not be found. Install it with the following command:"
+    echo ""
+    echo "    cargo install form"
+    echo ""
+    exit 1
+fi
+
 set -euxo pipefail
 
-rm -rf src
-mkdir src
+rm -rf src/nrf*
+rm -rf src/lib.rs
 
-(cd ../chiptool/; cargo build)
-RUST_BACKTRACE=1 RUST_LOG=info ../chiptool/target/debug/chiptool generate --svd svd/nrf52840.svd --transform svd/nrf52840.yaml
+declare -A identifiers=(
+    # Chip name to SVD/YAML/.X identifier
+    # This allows to have multiple chip definition with the same SVD/YAML/.X file-set (e.g. for the nrf5340-app chip)
+    ["nrf52840"]="nrf52840"
+)
+declare -A targets=(
+    # Chip name to CPU arch target
+    ["nrf52840"]="thumbv7em-none-eabihf"
+)
+chips=($(printf "%s\n" "${!identifiers[@]}" | sort))
 
-# cargo install form
-form -i lib.rs -o src
-rm lib.rs
-#mv lib.rs src
+# Generated types
+for chip in ${chips[@]}; do 
+    chip_identifier=${identifiers[$chip]}
+    chiptool generate --svd svd/$chip_identifier.svd --transform svd/$chip_identifier.yaml
+    rustfmt lib.rs
+    sed -i '/#!\[no_std]/d' lib.rs
+    form -i lib.rs -o src/$chip
+    mv src/$chip/lib.rs src/$chip/mod.rs
+    rm lib.rs
+done
 
+# Create build.rs from template
+chip_list=""
+for chip in ${chips[@]}; do
+    chip_identifier=${identifiers[$chip]}
+    chip_list+=$(printf '("%s", "%s"), ' "$chip" "$chip_identifier")
+done
+chip_list=${chip_list%, }   # Remove the trailing comma and space
+sed "s/CHIP_LIST/$chip_list/" build.rs.in > build.rs
+
+# Create Cargo.toml from template
+features_list=""
+flavors_list=""
+for chip in ${chips[@]}; do
+    chip_target=${targets[$chip]}
+    features_list+=$(printf '%s = []' "$chip")
+    features_list+="\n"
+    flavors_list+=$(printf '    { name = "%s", features = ["%s"], target = "%s" },' "$chip" "$chip" "$chip_target")
+    flavors_list+="\n"
+done
+flavors_list=${flavors_list%,\\n}   # Remove the trailing comma and newline
+features_list=${features_list%\\n}   # Remove the trailing newline
+
+awk -v features="$features_list" \
+    -v flavors="$flavors_list" \
+     '{gsub(/FEATURES/, features); gsub(/FLAVORS/, flavors)}1' Cargo.toml.in > Cargo.toml
+
+# Create src/lib.rs
+cfg_features=$(printf 'feature = "%s", ' "${chips[@]}")
+cfg_features=${cfg_features%, }   # Remove the trailing comma and space
+chip_cfg_attrs=""
+for chip in "${chips[@]}"; do
+    chip_cfg_attrs+=$(printf '#[cfg_attr(feature = "%s", path = "./%s/mod.rs")]' "$chip" "$chip")
+done
+
+# Perform the replacements in the template and save to `src/lib.rs`
+chip_list=$(printf '`%s`, ' "${chips[@]}")
+chip_list=${chip_list%, }   # Remove the trailing comma and space
+awk -v cfg_all="$cfg_features" \
+    -v cfg_any="$cfg_features" \
+    -v chip_list="$chip_list" \
+    -v chip_cfg_attrs="$chip_cfg_attrs" \
+    '{gsub(/CHIPS_CFG_ALL/, cfg_all); gsub(/CHIPS_CFG_ANY/, cfg_any); gsub(/CHIP_LIST/, chip_list); gsub(/CHIP_CFG_ATTRS/, chip_cfg_attrs)}1' src/lib.rs.in > src/lib.rs
+
+# Code formatting and sanity check
 cargo fmt
-cargo check
-cargo doc
+
+for chip in ${chips[@]}; do 
+    cargo check --features rt,$chip
+done
